c6b07d3e0b33979d81cae68f33acb354d6abb264
Nikita Kudrin <nik123.93@ya.ru>
2022-01-17 01:20:07 +0300

    add http request for syllables

--- src/main/java/com/nick/flipper/syllables/SyllableHttpRequestService.java
+++ src/main/java/com/nick/flipper/syllables/SyllableHttpRequestService.java
@@ -1,39 +1,47 @@
 package com.nick.flipper.syllables;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.NonNull;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Pattern;
 
 @Service
 public class SyllableHttpRequestService {
 
     private static final String BASE_URL = "https://slogi.su/word.json";
 
+    public String getSyllables(@NonNull final String query) throws IOException {
+        var url = new URL(BASE_URL);
+        var con = (HttpURLConnection) url.openConnection();
 
-    public String getSyllables(@NonNull final String query) {
-        var header = new HttpHeaders();
-        header.setContentType(MediaType.APPLICATION_JSON);
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.getOutputStream().write("q=собака".getBytes());
 
-        var bodyParams = new LinkedMultiValueMap<String, String>();
-        ;
-        bodyParams.add("q", URLEncoder.encode(query, StandardCharsets.UTF_8));
+        var content = new StringBuilder();
 
-        var requestEnty = new HttpEntity<>(bodyParams, header);
+        try (var in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                content.append(line);
+            }
+        }
+        con.disconnect();
 
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters()
-                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-        var result = restTemplate.exchange(BASE_URL, HttpMethod.POST, requestEnty, String.class);
-        return result.getBody();
+        var output = content.toString();
+
+        var pattern = Pattern.compile("(?<=<span class=word-syllables>)(.*)(?=</span>)");
+        var matcher = pattern.matcher(output);
+
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+
+        return "";
     }
 }
--- src/test/java/com/nick/flipper/FlipperApplicationTests.java
+++ src/test/java/com/nick/flipper/FlipperApplicationTests.java
@@ -1,10 +1,11 @@
 package com.nick.flipper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nick.flipper.syllables.SyllableHttpRequestService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class FlipperApplicationTests {
@@ -17,7 +18,7 @@ class FlipperApplicationTests {
     }
 
     @Test
-    void testRequest() throws JsonProcessingException {
+    void testRequest() throws IOException {
         var result = syllableHttpRequestService.getSyllables("собака");
     }
 
